204e00c602733888e5b982457814b65e71664499
Mont1k <104359868+Mont1k@users.noreply.github.com>
2022-08-12 23:27:06 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        Circle.area(num);
+        Circle.circumference(num);
     }
 }
