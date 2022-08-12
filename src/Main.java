c1b979bbaced401f83fe36111bd9273a345967b0
Nurtilek <nurtilekorozov@gmail.com>
2022-08-12 23:27:24 +0600

    ss

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        Circle.area(num);
+        Circle.circumference(num);
     }
-}
+    }
