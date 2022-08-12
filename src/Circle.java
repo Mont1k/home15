e3d2fe0c74ea77c9ba453d656ddf48dc95806c30
Mont1k <104359868+Mont1k@users.noreply.github.com>
2022-08-12 23:28:48 +0600

    Update Circle.java

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,11 @@
 package PACKAGE_NAME;public class Circle {
+  private static final double pi = Math.PI;
+
+    public static void area(double num) {
+        System.out.println(pi * num * num);
+    }
+
+    public static void circumference(double num) {
+        System.out.println((pi * 2) * num);
+    }
 }
